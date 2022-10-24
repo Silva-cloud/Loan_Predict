7757ad2be9b17b2aea7ce9b8e3d9c4a02ee5f255
Silva-cloud <80207026+Silva-cloud@users.noreply.github.com>
2022-10-24 21:29:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,12 +19,28 @@ def predict():
     LR_prediction = LRmodel.predict(data)
     DT_prediction = DTmodel.predict(data)
     NB_prediction = NBmodel.predict(data)
+
+
+    if(LR_prediction[0]== 1):
+        Labeled_LR_prediction= 'Yes'
+    elif(LR_prediction[0]== 0):
+        Labeled_LR_prediction= 'No'
+
+    if(DT_prediction[0]== 1):
+        Labeled_DT_prediction= 'Yes'
+    elif(DT_prediction[0]== 0):
+        Labeled_DT_prediction= 'No'
+
+    if(NB_prediction[0]== 1):
+        Labeled_NB_prediction= 'Yes'
+    elif(NB_prediction[0]== 0):
+        Labeled_NB_prediction= 'No'
+
     return render_template(
         'index.html', 
-        prediction_text= 
-        'Logistic Regression model says {}\n'.format(LR_prediction)+
-        'Decision Tree model says {}\n'.format(DT_prediction)+
-        'Naive Bayes model says {}'.format(NB_prediction))
+        Logistic_Regression_result= 'نتيجة توقع نموذج ال Logisitic Regression هي {}'.format(Labeled_LR_prediction),
+        Decision_Tree_result= 'نتيجة توقع نموذج ال Decision Tree هي {}'.format(Labeled_DT_prediction),
+        Naive_Bias_result= 'نتيجة توقع نموذج ال Naive Bias هي {}'.format(Labeled_NB_prediction))
 
 if __name__ == '__main__':
     app.run(debug=True)
